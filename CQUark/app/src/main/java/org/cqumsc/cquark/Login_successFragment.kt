95dfaa3906d7246af902693b5c157bb2b9ef19ca
Xiao-Anystring <15015148797@163.com>
2023-01-12 18:21:52 +0800

    finally

---
+++ CQUark/app/src/main/java/org/cqumsc/cquark/Login_successFragment.kt
@@ -0,0 +1,26 @@
+package org.cqumsc.cquark
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Login_successFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_success, container, false)
+    }
+
+}
